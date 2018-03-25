f152b112fd9f73c61fecf343443c7f9e387abe63
Catherine Looper <loopercat@gmail.com>
2018-03-24 18:39:04 -0700

    get route is working and returning songs

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-// import ReactDom from 'react-dom';
-// import App from './component/app/index.js';
-// import './style/main.scss';
+import ReactDom from 'react-dom';
+import App from './src/component/app/index.js';
+import './src/style/main.scss';
 
-// const container = document.createElement('div');
-// document.body.appendChild(container);
-// ReactDom.render(<App />, container);
-
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDom.render(<App />, container);
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './component/app';
-import './style/main.scss';
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import App from './component/app';
+// import './style/main.scss';
 
-const container = document.createElement('div');
-document.body.appendChild(container);
-ReactDom.render(<App />, container);
+// const container = document.createElement('div');
+// document.body.appendChild(container);
+// ReactDom.render(<App />, container);
 
