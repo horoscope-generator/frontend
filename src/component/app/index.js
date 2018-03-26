29fb8bdd827b253fb853fac6ce4ea979d85955b5
Catherine Looper <loopercat@gmail.com>
2018-03-26 15:32:45 -0700

    adding variables into the libra template

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -7,15 +7,35 @@ const __API_URL__ = process.env.API_URL;
 class App extends React.Component {
   render() {
 
+    let data;
+    let dataArray;
+
     superagent.get(`${__API_URL__}/lyrics`)
       .then(songObject => {
-        console.log('songObject', JSON.parse(songObject.text).result[0].album);
-        let data = JSON.parse(songObject.text).result[0].lyrics;
+        data = JSON.parse(songObject.text).result[0].lyrics;
+        dataArray = data.split(' ');
+        console.log('data', data);
+        console.log('dataArray', dataArray);
+        // let container = document.createElement('div');
+        // container.textContent = dataArray;
+        // document.body.appendChild(container);
+
+        let randomIndex1 = Math.floor(Math.random() * dataArray.length);
+        let randomIndex2 = Math.floor(Math.random() * dataArray.length);
+        
+        let sign = 'Libra';
+        let word1 = dataArray[randomIndex1];
+        let word2 = dataArray[randomIndex2];
+        console.log('word1', word1);
+        console.log('word2', word2);
+        
+        
+        let horoscope = `Assessment of others' ideas may be a main focus today, ${sign}. Group ${word1} could require your intellectual input. A ${word2} could come to you with an idea for a project of some kind. Writing or speaking may be involved in some way. Your friend isn't in a very practical frame of mind, so don't be surprised if the ideas don't seem very workable right now. Don't be discouraging, however. Your friend may surprise you!`;
+        
         let container = document.createElement('div');
-        container.textContent = data;
+        container.textContent = horoscope;
         document.body.appendChild(container);
       });
-      
 
     return(
       <div className='app'>
